429245e0fafe767fe4331f7505191cec8d4bf985
ArnoldoCortez <arnoldo.cortez@theksquaregroup.com>
2021-06-07 15:29:03 -0500

    adding more details and fetching characters data

--- src/components/details-screen.component.js
+++ src/components/details-screen.component.js
@@ -1,16 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import './details-screen.styles.css';
 
 function DetailScreen({ episode, title, opening, director, producer, characters }) {
+
+  const [listCharacters, setListCharacters] = useState([]);
+
+  useEffect(() => {
+    if(episode) {
+      const urlList = characters.map((character) => axios.get(character));
+      axios.all(urlList)
+      .then(response => response.map((character) => character.data.name))
+      .then(names => setListCharacters(names)); 
+    }
+  }, [episode, characters]);
 
   return (
     <div className='screen-container'>
       <div className='screen'>
         { episode ?
-          <div className='details'>
+          <div key={episode} className='details'>
             <h3>{ ` Episode ${ episode }` }</h3>
             <h1>{ title }</h1>
             <p>{ opening }</p>
-        </div> : null
+            <span>{ `Directed by ${ director }` }</span><br/>
+            <span>{ `Produced by ${ producer }` }</span>
+            <h4>Characters:</h4>
+            {listCharacters.map((character, i) => <span key={ i }>{ character }</span>)}
+          </div> : null
         }
       </div>
     </div>
